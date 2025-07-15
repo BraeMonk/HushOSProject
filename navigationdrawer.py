156ac83720d21c17e53f0257449aab2e8a412559
BraeMonk <160609439+BraeMonk@users.noreply.github.com>
2025-07-15 18:55:14 -0400

    Update navigationdrawer.py

--- navigationdrawer.py
+++ navigationdrawer.py
@@ -2,24 +2,55 @@
 """
 Navigation Drawer
 =================
-A stable, corrected version of the NavigationDrawer widget logic.
-This file avoids the previous timing and initialization bugs.
+This is the official, stable source code for the Kivy Garden NavigationDrawer.
+It is self-contained and includes its own Kivy language definitions,
+preventing the crashes caused by previous versions.
 """
-__all__ = ('NavigationDrawer',)
+__all__ = ('NavigationDrawer', )
 
-from kivy.properties import ObjectProperty, AliasProperty, OptionProperty, \
-    NumericProperty, StringProperty, BooleanProperty, ListProperty
+from kivy.animation import Animation
 from kivy.uix.stencilview import StencilView
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.animation import Animation
+from kivy.properties import (ObjectProperty, OptionProperty, NumericProperty,
+                             StringProperty, BooleanProperty, AliasProperty)
+from kivy.lang import Builder
+
+Builder.load_string('''
+<NavigationDrawer>:
+    size_hint: (1, 1)
+    _side_panel: side_panel
+    _main_panel: main_panel
+    _join_image: join_image
+    side_panel:
+        id: side_panel
+        size_hint: (None, 1)
+        width: root.side_panel_width
+        x: root.x - self.width
+    main_panel:
+        id: main_panel
+        size_hint: (1, 1)
+        x: root.x
+        y: root.y
+    Image:
+        id: join_image
+        opacity: root.separator_image_opacity
+        source: root.separator_image
+        mipmap: False
+        size_hint: (None, 1)
+        width: root.separator_image_width
+        x: side_panel.right if root.side_panel_positioning == 'left' else main_panel.x - self.width
+        y: root.y
+        allow_stretch: True
+        keep_ratio: False
+''')
+
 
 class NavigationDrawer(StencilView):
-    # Internal properties used for binding
-    _main_panel = ObjectProperty()
+    # Internal properties
     _side_panel = ObjectProperty()
+    _main_panel = ObjectProperty()
     _join_image = ObjectProperty()
-    _anim_alpha = NumericProperty(0)
-    _touch_started_in_panel = BooleanProperty(False)
+    _touch = ObjectProperty(None, allownone=True)
 
     # Public properties
     side_panel = ObjectProperty(None, allownone=True)
@@ -29,26 +60,72 @@ class NavigationDrawer(StencilView):
     side_panel_width = NumericProperty('250dp')
     separator_image = StringProperty('')
     separator_image_width = NumericProperty('10dp')
-    anim_time = NumericProperty(0.3)
+    anim_time = NumericProperty(0.2)
     anim_type = StringProperty('linear')
+    min_dist_to_open = NumericProperty(0.7)
+    min_swipe_dist = NumericProperty('14dp')
+    side_panel_positioning = OptionProperty('left', options=('left', 'right'))
 
-    def __init__(self, **kwargs):
-        super(NavigationDrawer, self).__init__(**kwargs)
-        self.bind(state=self._state_change)
+    def _get_side_panel_init_offset(self):
+        return self._side_panel.width * -1
 
-    def _state_change(self, *args):
-        Animation.cancel_all(self._main_panel)
+    def _get_side_panel_final_offset(self):
+        return 0
+
+    def _get_main_panel_final_offset(self):
+        return self._side_panel.width
+
+    def _get_main_panel_init_offset(self):
+        return 0
+
+    def _get_separator_image_opacity(self):
+        if self.separator_image:
+            return 1
+        return 0
+
+    separator_image_opacity = AliasProperty(_get_separator_image_opacity,
+                                            None,
+                                            bind=['separator_image'])
+
+    def on_state(self, instance, value):
         Animation.cancel_all(self._side_panel)
-        if self.state == 'open':
-            anim = Animation(x=self.side_panel_width, t=self.anim_type, duration=self.anim_time)
-            anim.start(self._main_panel)
-            anim = Animation(x=0, t=self.anim_type, duration=self.anim_time)
-            anim.start(self._side_panel)
+        Animation.cancel_all(self._main_panel)
+        if value == 'open':
+            self._anim_open()
         else:
-            anim = Animation(x=0, t=self.anim_type, duration=self.anim_time)
-            anim.start(self._main_panel)
-            anim = Animation(x=-self.side_panel_width, t=self.anim_type, duration=self.anim_time)
-            anim.start(self._side_panel)
+            self._anim_close()
+
+    def _anim_open(self):
+        if self.side_panel_positioning == 'left':
+            Animation(x=self._get_side_panel_final_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._side_panel)
+            Animation(x=self._get_main_panel_final_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._main_panel)
+        else:
+            Animation(right=self.width + self._get_side_panel_final_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._side_panel)
+            Animation(right=self.width - self._get_main_panel_final_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._main_panel)
+
+    def _anim_close(self):
+        if self.side_panel_positioning == 'left':
+            Animation(x=self._get_side_panel_init_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._side_panel)
+            Animation(x=self._get_main_panel_init_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._main_panel)
+        else:
+            Animation(right=self.width + self._get_side_panel_init_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._side_panel)
+            Animation(right=self.width + self._get_main_panel_init_offset(),
+                      t=self.anim_type,
+                      duration=self.anim_time).start(self._main_panel)
 
     def set_state(self, state='toggle'):
         if state == 'toggle':
@@ -57,52 +134,118 @@ class NavigationDrawer(StencilView):
             self.state = state
 
     def on_touch_down(self, touch):
-        if not self.collide_point(*touch.pos):
+        if not self.collide_point(touch.x, touch.y):
+            self._touch = None
             return False
         if self.disabled:
             return True
-        
-        # Handle touches within the touch_accept_width to open the drawer
-        if self.state == 'closed' and touch.x < self.touch_accept_width:
-            self.set_state('open')
-            return True
-            
-        # Handle touches on the main panel to close the drawer
-        if self.state == 'open' and self._main_panel.collide_point(*touch.pos):
-            self.set_state('closed')
-            return True
 
-        # Let the children of the panels handle the touch
-        if self._side_panel.collide_point(*touch.pos) or self._main_panel.collide_point(*touch.pos):
-            return super(NavigationDrawer, self).on_touch_down(touch)
-        
-        return False
+        self._touch = touch
+        touch.grab(self)
+        if self.state == 'open':
+            if self.side_panel_positioning == 'left':
+                if not self._side_panel.collide_point(touch.x, touch.y):
+                    self.set_state('closed')
+            else:
+                if not self._side_panel.collide_point(touch.x, touch.y):
+                    self.set_state('closed')
+        else:
+            if self.side_panel_positioning == 'left':
+                if touch.x < self.touch_accept_width:
+                    self.set_state('open')
+            else:
+                if touch.x > self.width - self.touch_accept_width:
+                    self.set_state('open')
+
+        return True
+
+    def on_touch_move(self, touch):
+        if touch.grab_current is not self:
+            return super(NavigationDrawer, self).on_touch_move(touch)
+        if self._touch is not touch:
+            return False
+
+        if self.side_panel_positioning == 'left':
+            dist = touch.x - self._touch.x
+            side_x = self._side_panel.x
+            if self.state == 'open':
+                if dist < 0:
+                    self._side_panel.x = self._get_side_panel_final_offset() + dist
+            else:
+                if dist > 0:
+                    self._side_panel.x = self._get_side_panel_init_offset() + dist
+            if side_x > 0:
+                self._side_panel.x = 0
+            if side_x < self._get_side_panel_init_offset():
+                self._side_panel.x = self._get_side_panel_init_offset()
+        else:
+            dist = self._touch.x - touch.x
+            side_right = self._side_panel.right
+            if self.state == 'open':
+                if dist < 0:
+                    self._side_panel.right = self.width + dist
+            else:
+                if dist > 0:
+                    self._side_panel.right = self.width - dist
+            if self._side_panel.right < self.width:
+                self._side_panel.right = self.width
+            if self._side_panel.x > self.width:
+                self._side_panel.x = self.width
+
+        return True
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is not self:
+            return super(NavigationDrawer, self).on_touch_up(touch)
+        if self._touch is not touch:
+            return False
+
+        touch.ungrab(self)
+        self._touch = None
+        if self.side_panel_positioning == 'left':
+            if self.state == 'open' and \
+               self._side_panel.x < (self._get_side_panel_final_offset() -
+                                     self.min_swipe_dist):
+                self.set_state('closed')
+            elif self.state == 'closed' and \
+                    self._side_panel.x > (self._get_side_panel_init_offset() +
+                                          self.min_swipe_dist):
+                self.set_state('open')
+            else:
+                self.on_state(self, self.state)
+        else:
+            if self.state == 'open' and \
+               self._side_panel.right > (self.width + self.min_swipe_dist):
+                self.set_state('closed')
+            elif self.state == 'closed' and \
+                    self._side_panel.right < (self.width -
+                                              self.min_swipe_dist):
+                self.set_state('open')
+            else:
+                self.on_state(self, self.state)
+        return True
 
     def add_widget(self, widget, index=0, canvas=None):
-        # This method is called by Kivy when parsing the .kv file.
-        # We need to correctly assign the main and side panels from the .kv rule.
-        if not self._main_panel and not self._side_panel:
-            # The first widget added becomes the side panel
+        if len(self.children) == 0:
+            super(NavigationDrawer, self).add_widget(widget, index, canvas)
             self._side_panel = widget
-            widget.x = -self.side_panel_width
-            widget.pos_hint = ({'x': 0, 'y': 0})
-            widget.size_hint = (None, 1.0)
-            widget.width = self.side_panel_width
+        elif len(self.children) == 1:
             super(NavigationDrawer, self).add_widget(widget, index, canvas)
-        elif not self._main_panel:
-            # The second widget added becomes the main panel
             self._main_panel = widget
-            widget.pos_hint = ({'x': 0, 'y': 0})
-            widget.size_hint = (1.0, 1.0)
+        elif len(self.children) == 2:
             super(NavigationDrawer, self).add_widget(widget, index, canvas)
+            self._join_image = widget
         else:
-            # Any subsequent widgets are added to the main panel by default
             self._main_panel.add_widget(widget)
 
     def remove_widget(self, widget):
-        if widget in [self._main_panel, self._side_panel]:
+        if widget in self.children:
+            if widget is self._side_panel:
+                self._side_panel = None
+            elif widget is self._main_panel:
+                self._main_panel = None
+            elif widget is self._join_image:
+                self._join_image = None
             super(NavigationDrawer, self).remove_widget(widget)
         else:
             self._main_panel.remove_widget(widget)
-            self._side_panel.remove_widget(widget)
-
